2a59aa4fac3b503aa646940600ab1bf4855060e2
Tauan Camargo <lukyhenson@gmail.com>
2024-05-14 12:31:16 -0300

    feat: add get note data

--- note.go
+++ note.go
@@ -1,7 +1,32 @@
 package main
 
-import "example.com/note/user"
+import (
+	"fmt"
+
+	"example.com/note/user"
+)
+
+func getNoteData() (string, string, error) {
+    title, error := user.GetUserInput("Enter note title: ")
+    
+    if error != nil {
+        return "", "", error
+    }
+    
+    content, error := user.GetUserInput("Enter note content: ")
+
+    if error != nil {
+        return "", "", error
+    }
+
+    return title, content, nil
+}
 
 func main() {
-    user.GetUserInput("Enter note title: ")
+    title, content, error := getNoteData()
+
+    if error != nil {
+        fmt.Println(error)
+        return
+    }
 }
