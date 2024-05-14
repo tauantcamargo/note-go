dbf5df82fc5cb24962b3e78089dac238a4c757da
Tauan Camargo <lukyhenson@gmail.com>
2024-05-14 13:06:51 -0300

    feat: use bufio to read the input of user for long user input

--- user/user.go
+++ user/user.go
@@ -1,12 +1,23 @@
 package user
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func GetUserInput(prompt string) string {
     fmt.Print(prompt)
-    var value string
-    fmt.Scanln(&value)
-    return value
+    reader := bufio.NewReader(os.Stdin)
+    text, error := reader.ReadString('\n')
+
+    if error != nil {
+        return ""
+    }
+
+    text = strings.TrimSuffix(text, "\n")
+    text = strings.TrimSuffix(text, "\r")
+
+    return text
 }
