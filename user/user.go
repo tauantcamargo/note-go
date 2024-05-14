d913e848d635430d9ebbcde66d151d7799a86e81
Tauan Camargo <lukyhenson@gmail.com>
2024-05-14 12:43:37 -0300

    refactor: update GetUserInput to not handle error

--- user/user.go
+++ user/user.go
@@ -1,18 +1,12 @@
 package user
 
 import (
-	"errors"
 	"fmt"
 )
 
-func GetUserInput(prompt string) (string, error) {
+func GetUserInput(prompt string) string {
     fmt.Print(prompt)
     var value string
     fmt.Scanln(&value)
-    
-    if value == "" {
-        return "", errors.New("error: Input cannot be empty")
-    }
-
-    return value, nil
+    return value
 }
