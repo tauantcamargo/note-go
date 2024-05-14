c55b7696d0c081629192d77b82679c70fb036daf
Tauan Camargo <lukyhenson@gmail.com>
2024-05-14 13:16:24 -0300

    feat: update to add Save method to a file as a json

--- note/note.go
+++ note/note.go
@@ -1,8 +1,11 @@
 package note
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"os"
+	"strings"
 	"time"
 
 	"example.com/note/user"
@@ -26,8 +29,21 @@ func New(title, content string) (*Note, error) {
     }, nil
 }
 
-func (note *Note) Display()  {
+func (note Note) Display()  {
     fmt.Printf("Your note titled %v has the following content:\n\n%v", note.title, note.content)
+}
+
+func (note Note) Save() error {
+    fileName := strings.ReplaceAll(note.title, " ", "_")
+    fileName = strings.ToLower(fileName)
+
+    json, err := json.Marshal(note)
+    
+    if err != nil {
+        return err
+    }
+
+    return os.WriteFile(fileName, json, 0644)
 }
 
 func GetNoteData() (string, string) {
